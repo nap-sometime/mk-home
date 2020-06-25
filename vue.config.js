0f37b07f58a6beb562a34cbd1495fe013ea77075
worasitipon <worasitipon.t@hotmail.com>
2020-06-25 12:11:13 +0700

    update vue config

--- vue.config.js
+++ vue.config.js
@@ -2,25 +2,17 @@ const path = require('path');
 const { readFileSync } = require('fs');
 const { name: npm_package_name } = require('./package.json');
 
+// pass thought client env via VUE_APP_*
 process.env.VUE_APP_NAME = npm_package_name;
 
-module.exports = {
-	lintOnSave: false,
-	filenameHashing: false,
-	productionSourceMap: false,
-	configureWebpack: {
-		output: {
-			path: path.resolve(__dirname, 'dist'),
-			chunkFilename: 'js/[name].bundle.js',
-		},
-		externals: ['vue', 'vue-router', 'js-cookie', 'vue-meta', /^@mk\/.+/],
-		resolve: {
-			alias: {
-				'@': path.resolve(__dirname, 'src'),
-				'~': path.resolve(__dirname, 'src'),
-			},
-		},
-		devServer: {
+// helper
+const isProd = ['production', 'runtest'].some((v) => process.env.NODE_ENV === v);
+const loadFile = (path = '') => readFileSync(path);
+
+// config webpack dev server
+const devServer = isProd
+	? undefined
+	: {
 			headers: {
 				'Access-Control-Allow-Origin': '*',
 			},
@@ -29,9 +21,27 @@ module.exports = {
 			sockHost: 'localhost',
 			port: 8002,
 			https: {
-				cert: readFileSync(path.join(__dirname, '../cert/cert.pem')),
-				key: readFileSync(path.join(__dirname, '../cert/key.pem')),
+				cert: loadFile(path.join(__dirname, '../cert/cert.pem')),
+				key: loadFile(path.join(__dirname, '../cert/key.pem')),
+			},
+	  };
+
+module.exports = {
+	lintOnSave: false,
+	filenameHashing: false,
+	productionSourceMap: true,
+	configureWebpack: {
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			chunkFilename: 'js/_[name].js',
+		},
+		externals: ['vue', 'vue-router', 'js-cookie', 'vue-meta', /^@mk\/.+/],
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, 'src'),
+				'~': path.resolve(__dirname, 'src'),
 			},
 		},
+		devServer,
 	},
 };
